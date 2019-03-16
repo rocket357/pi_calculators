0fee2f13fd747d1f6a8982648b30cd245bb9e026
Jonathon Sisson <sisson.j@gmail.com>
2019-03-16 04:05:48 -0700

    Update uniform_distribution.py
    Revert thread changes

--- uniform_distribution.py
+++ uniform_distribution.py
@@ -1,35 +1,6 @@
 #!/usr/bin/env python
 
-import sys, os, math, time
-import multiprocessing
-cores = multiprocessing.cpu_count()
-from threading import Thread
-import threading
-
-class pithread(Thread):
-        work = {}
-        divisions = 0
-        isRunning = False
-        
-        def __init__(self, divisions):
-                Thread.__init__(self)
-                self._stop_event = threading.Event()
-                
-        def run(self):
-                self.isRunning = True
-                while True:
-                        done = True
-                        if len(self.work.keys()) < 1:
-                                time.sleep(1)
-                        for opp in self.work.keys():
-                                if self.work[opp] == '':
-                                        print "Processing %s" % opp
-                                        done = False
-                                        adj = math.cos(math.asin(opp))
-                                        self.work[opp] = divisions - int(adj*divisions)
-                        if done:
-                                self.isRunning = False
-                                return
+import sys, os, math
 
 # as "spacing" approaches zero, accuracy goes up (as does required time)
 # based on sys.float_info.min, which on amd64/python 2.7.16 is ~2.22507 x 10^-308
@@ -40,34 +11,23 @@ divisions = int(1/(float(sys.float_info.min)/10**spacing))
 samples = divisions**2
 
 print "Current settings will give %s divisions from 0.0 to 1.0 for a sample size of %s" % (divisions, samples)
-print "Starting %s threads to perform calculations..." % cores
 
 miss = 0
+
 i = step
-
-# create threads
-threads = {}
-for j in range(0,cores):
-        worker = pithread(divisions)
-        threads[j] = worker
-
-# distribute work
 while i < 1.0:
-        for j in range(0, cores):
-                threads[j].work[i] = ''
-                i = i + step
+        # we have hyp = 1, opp = i
+        # thus asin(i) is the radians
+        # and cos(radians) is the adj (since hyp = 1)
+        adj = math.cos(math.asin(i))
+        
+        # since we know this, we can programatically determine the number of misses
+        miss = miss + divisions - int(adj*divisions)
+        
+        # I need to see output sometimes.
+        #print "opp: %s, adj: %s, misses: %s" % (i, adj, miss)
+        i = i + step
 
-for j in range(0,cores):
-        threads[j].start()
-                
-# work is distributed, now we need to collect the data
-while len(threads) > 0:
-        for j in threads.keys():
-                if not threads[j].isRunning:
-                        print sum(threads[j].work.values())
-                        miss = miss + sum(threads[j].work.values())
-                        del threads[j]
-                
 print "samples: %s misses: %s" % (samples, miss)
 
 p = ((samples - miss) / float(samples))*4   # like monte carlo integration, but we're intentionally making a uniform sample space.
